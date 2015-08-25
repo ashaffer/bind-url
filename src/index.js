c0e455f7ac1ab794cefc4fbdd149f90a807258ae
Andrew Shaffer <darawk@gmail.com>
2015-08-25 01:55:52 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+/**
+ * Imports
+ */
+
+import catchLinks from 'catch-links'
+
+/**
+ * Bind
+ */
+
+function bindUrl ({wnd, root}, cb) {
+  if (root === undefined) {
+    root = wnd
+  }
+
+  catchLinks(root, pushState)
+  wnd.addEventListener('popstate', update)
+
+  // Initialize
+  update()
+
+  function update () {
+    const {pathname, search} = wnd.location
+    cb([pathname, search].filter(Boolean).join('?'))
+  }
+
+  function pushState (url) {
+    wnd.history.pushState({}, '', url)
+    cb(url)
+  }
+}
+
+/**
+ * Exports
+ */
+
+export default bindUrl
